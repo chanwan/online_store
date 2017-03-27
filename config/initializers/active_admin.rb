fe1d9c105cdb69a7f6aba772cee59916c62fe6e9
Dair <dairmakeshov@gmail.com>
2017-03-27 14:04:52 +0600

    Installing gem devise

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -46,7 +46,7 @@ ActiveAdmin.setup do |config|
   # This will ONLY change the title for the admin section. Other
   # namespaces will continue to use the main "site_title" configuration.
 
-  == User Authentication
+  # == User Authentication
   #
   # Active Admin will automatically call an authentication
   # method in a before filter of all controller actions to
